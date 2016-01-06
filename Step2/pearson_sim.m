b893fe86ad66e411bbf7aef53adab694d887cbcd
Josh Ouyang <JoshOY789@gmail.com>
2016-01-06 21:26:11 +0800

    Add Pearson correlation coefficient calculation function.

---
+++ Step2/pearson_sim.m
@@ -0,0 +1,23 @@
+%% Final Project - Problem2 - pearson_sim.m
+% Created by 1352847 Junpeng Ouyang on Jan 6, 2015. All rights reserved.
+% Reference: https://segmentfault.com/q/1010000000094674
+
+function [ sim ] = pearson_sim( X, Y )
+%PEARSON_SIM Summary of this function goes here
+%   Detailed explanation goes here
+    assert(length(X) == length(Y), 'The length of X and Y in person_sim must be same!');
+    
+    avg_X = mean(X);
+    avg_Y = mean(Y);
+    
+    numerator = sum((X - avg_X) .* (Y - avg_Y));
+    
+    rxms = sum( (X - avg_X) .^ 2 ) ^ 0.5;
+    ryms = sum( (Y - avg_Y) .^ 2 ) ^ 0.5;
+    
+    denominator = rxms * ryms;
+    
+    sim = numerator / denominator;
+    
+end
+
